3f1498d73ebbe249c4a81fa29ce0f241efbe38d2
Jeet Mahaur <132244263+jeetmahaur7@users.noreply.github.com>
2025-06-12 13:14:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,9 @@
-// import "./App.css";
-// import Faq from "./components/faq/Faq";
-
-// function App() {
-//   return (
-//     <>
-//       <Faq />
-//     </>
-//   );
-// }
-// export default App;
-
-// import "./App.css";
-// import Counter from "./components/counter/Counter";
-
-// function App() {
-//   return (
-//     <>
-//       <Counter />
-//     </>
-//   );
-// }
-// export default App;
-
-// import "./App.css";
-// import Calculator from "./components/calculator/Calculator";
-// function App() {
-//   return (
-//     <>
-//       <Calculator />
-//     </>
-//   );
-// }
-// export default App;
-
 import "./App.css";
-import CustomeModal from "./components/modal/CustomeModal";
+import Calculator from "./components/calculator/Calculator";
 function App() {
   return (
     <>
-      <CustomeModal />
+      <Calculator />
     </>
   );
 }
